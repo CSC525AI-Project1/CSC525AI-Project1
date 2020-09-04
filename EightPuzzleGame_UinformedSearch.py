c09e40438d8b18ebbc203fe35a3880a9c7d64735
Trevor <48963638+tapplegate@users.noreply.github.com>
2020-09-04 14:22:04 -0400

    Update EightPuzzleGame_UinformedSearch.py
    Uninformed search is now working!
    Removed or commented out many debugging statements
    Cleaned up the function that searched the closed list and compared the stated created by swap to see if it is an already visited state.
    Fixed a tabbing issue in the if statements that preformed swaps.  Swap Right could only execute if a swap down happened first because it was tabbed into the swap down if, correcting this fixed uninformed search.  Huzzah the project is 30% complete.

--- EightPuzzleGame_UinformedSearch.py
+++ EightPuzzleGame_UinformedSearch.py
@@ -1,5 +1,6 @@
 import numpy as np
 from EightPuzzleGame_State import State
+import copy
 
 '''
 This class implement one of the Uinformed Search algorithm
@@ -22,27 +23,38 @@ class UninformedSearchSolver:
 
     def check_inclusive(self, s):
         """#TODO your code start here"""
-    """code for checking if the current state s is already a visited state
-    in the list of closed states"""
+        """code for checking if the current state s is already a visited state
+        in the list of closed states"""
 
-    """a boolen for determining if a state is already visited, default to true"""
-    sameState = true
+        """a boolen for determining if a state is already visited, default to false"""
+        sameState = False
 
-    """Code to iterate through every element in the closed list"""
-    for states in range(len(closed)):
-        """code to iterate through the x values of the current element in the closed list"""
-        for x in range(len(closed[states])):
-            """code ot iterate through the y values of the current element in the closed list"""
-            for y in range(len(closed[x])):
-                """if the current element in the x y position of closed is NOT equal to the
-                value in the same x y position in the passed state s then the two are different,
-                and state s is not currently in the closed list, and should be added"""
-                if closed.current[x][y] != s[x][y]:
-                    sameState = false
+        print("I am checking if this state is closed")
 
-    """if the current state s is not found in the closed list, add it to the closed list"""
-    if sameState == false:
-        closed.push(s)
+        """For loop that checks every state in the closed list against the state passed
+        by the swap function, if they are the same sets a tracker book to true and does nothing
+        if the passed state s is not found in closed, adds it to closed and the open list"""
+        for states in self.closed:
+            if (states.tile_seq== s.tile_seq).all() == True:
+                print("The states are the same")
+                sameState = True
+                """ if a.all(states.tile_seq) == s.tile_seq:
+               print("The states are the same!")
+               sameState = True"""
+
+
+        """if the current state s is not found in the closed list, add it to the closed list
+        and add it to the open list to progress the search"""
+        if sameState == False:
+            print("The states are different!")
+            tempMatrixClosed = copy.deepcopy(s)
+            tempMatrixOpen = copy.deepcopy(s)
+
+            self.closed.append(tempMatrixClosed)
+            self.openlist.append(tempMatrixOpen)
+
+        print("There are this many states in closed ", len(self.closed))
+        print("There are this many states in open ", len(self.openlist))
 
 
 
@@ -60,85 +72,121 @@ class UninformedSearchSolver:
     def state_walk(self):
         """#TODO your code start here"""
 
-    """create variables for the x and y coordinates of the 0 character"""
-    zeroLocationXCord = 0
-    zeroLocationYCord = 0
+        """create variables for the x and y coordinates of the 0 character"""
+        zeroLocationXCord = 0
+        zeroLocationYCord = 0
+        xPosition = 0
+        yPosition = 0
 
-    """find the position of the 0 character
-    iterate through the multidimensional array until you find the 0 char
-    then set zeroLocationX and Y coordinates variables"""
-    for x in range(len(current)):
-        for y in range(len(current[x])):
-            if self.current[x][y] == 0:
-                zeroLocationXCord = x
-                zeroLocationYCord = y
+        """find the position of the 0 character
+        iterate through the multidimensional array until you find the 0 char
+        then set zeroLocationX and Y coordinates variables"""
 
-    """if statements checking for each valid directional walk and performing
-    the swap if it is a valid walk"""
+        for rows in self.current.tile_seq:
+            for cells in rows:
+                if cells == 0:
+                    zeroLocationXCord = xPosition
+                    zeroLocationYCord = yPosition
+                yPosition = yPosition + 1
+            xPosition = xPosition + 1
+            yPosition = 0
 
-    """if x cord is greater than 0, you can perform a left swap"""
-    if zeroLocationXCord > 0:
-        """create a temp matrix to overwrite when performing the swap"""
-        tempMatrix = current
-        """Create a variable to hold the number of the position where zero is moving to"""
-        temp = tempMatrix[zeroLocationXCord-1][zeroLocationYCord]
-        """set the number at that position to zero"""
-        tempMatrix[zeroLocationXCord-1][zeroLocationYCord] = 0
-        """replace the 0 in the old position with the temp number, completing the swap"""
-        tempMatrix[zeroLocationXCord][zeroLocationYCord] = temp
-
-        """check if the new state is already in the visited states closed list"""
-        checkInclusive(tempMatrix)
-
-    """if x cord is less than 3, you can perform a right swap"""
-    if zeroLocationXCord < 3:
-        """create a temp matrix to overwrite when performing the swap"""
-        tempMatrix = current
-        """Create a variable to hold the number of the position where zero is moving to"""
-        temp = tempMatrix[zeroLocationXCord + 1][zeroLocationYCord]
-        """set the number at that position to zero"""
-        tempMatrix[zeroLocationXCord + 1][zeroLocationYCord] = 0
-        """replace the 0 in the old position with the temp number, completing the swap"""
-        tempMatrix[zeroLocationXCord][zeroLocationYCord] = temp
-
-        """check if the new state is already in the visited states closed list"""
-        checkInclusive(tempMatrix)
-
-    """if y cord is greater than 0, you can perform an up swap"""
-    if zeroLocationYCord > 0:
-        """create a temp matrix to overwrite when performing the swap"""
-        tempMatrix = current
-        """Create a variable to hold the number of the position where zero is moving to"""
-        temp = tempMatrix[zeroLocationXCord][zeroLocationYCord-1]
-        """set the number at that position to zero"""
-        tempMatrix[zeroLocationXCord][zeroLocationYCord-1] = 0
-        """replace the 0 in the old position with the temp number, completing the swap"""
-        tempMatrix[zeroLocationXCord][zeroLocationYCord] = temp
-
-        """check if the new state is already in the visited states closed list"""
-        checkInclusive(tempMatrix)
-
-    """if y cord is less than 3, you can perform an up swap"""
-    if zeroLocationYCord < 3:
-        """create a temp matrix to overwrite when performing the swap"""
-        tempMatrix = current
-        """Create a variable to hold the number of the position where zero is moving to"""
-        temp = tempMatrix[zeroLocationXCord][zeroLocationYCord + 1]
-        """set the number at that position to zero"""
-        tempMatrix[zeroLocationXCord][zeroLocationYCord + 1] = 0
-        """replace the 0 in the old position with the temp number, completing the swap"""
-        tempMatrix[zeroLocationXCord][zeroLocationYCord] = temp
-
-        """check if the new state is already in the visited states closed list"""
-        checkInclusive(tempMatrix)
+        """for (i,row) in self.current.tile_seq:
+            for (z,value) in self.current.tile_seq(row):
+                if z == 0:
+                    zeroLocationXCord=i
+                    zeroLocationYCord=z"""
 
 
 
+        """if statements checking for each valid directional walk and performing
+        the swap if it is a valid walk"""
+
+        print("I am entering the swapping phase")
+        """if x cord is greater than 0, you can perform an up swap"""
+        if zeroLocationXCord > 0:
+            print("I am doing an up swap")
+            print(zeroLocationXCord)
+            print(zeroLocationYCord)
+            """print(self.current.tile_seq)"""
+            """create a temp matrix to overwrite when performing the swap"""
+            tempMatrix = copy.deepcopy(self.current)
+            """Create a variable to hold the number of the position where zero is moving to"""
+            temp = tempMatrix.tile_seq[zeroLocationXCord-1][zeroLocationYCord]
+            """print("Current temp is:")
+            print(temp)"""
+            """set the number at that position to zero"""
+            tempMatrix.tile_seq[zeroLocationXCord-1][zeroLocationYCord] = 0
+            """replace the 0 in the old position with the temp number, completing the swap"""
+            tempMatrix.tile_seq[zeroLocationXCord][zeroLocationYCord] = temp
+            """print(tempMatrix.tile_seq)"""
+            """self.openlist.append(tempMatrix)"""
+            """check if the new state is already in the visited states closed list"""
+            self.check_inclusive(tempMatrix)
+
+        """if y cord is less than 3, you can perform a down swap"""
+        if zeroLocationXCord < 2:
+            print("I am doing a down swap")
+            print(zeroLocationXCord)
+            print(zeroLocationYCord)
+            """print(self.current.tile_seq)"""
+            """create a temp matrix to overwrite when performing the swap"""
+            tempMatrix = copy.deepcopy(self.current)
+            """Create a variable to hold the number of the position where zero is moving to"""
+            temp = tempMatrix.tile_seq[zeroLocationXCord + 1][zeroLocationYCord]
+            """print("Current temp is:")
+            print(temp)"""
+            """set the number at that position to zero"""
+            tempMatrix.tile_seq[zeroLocationXCord + 1][zeroLocationYCord] = 0
+            """replace the 0 in the old position with the temp number, completing the swap"""
+            tempMatrix.tile_seq[zeroLocationXCord][zeroLocationYCord] = temp
+            """print(tempMatrix.tile_seq)"""
+            """self.openlist.append(tempMatrix)"""
+            self.check_inclusive(tempMatrix)
 
 
-       
-       
-       
+        """if Y cord is less than 2, you can perform a right swap"""
+        if zeroLocationYCord < 2:
+            print("I am doing a right swap")
+            print(zeroLocationXCord)
+            print(zeroLocationYCord)
+            """print(self.current.tile_seq)"""
+            """create a temp matrix to overwrite when performing the swap"""
+            tempMatrix = copy.deepcopy(self.current)
+            """Create a variable to hold the number of the position where zero is moving to"""
+            temp = tempMatrix.tile_seq[zeroLocationXCord][zeroLocationYCord + 1]
+            """print("Current temp is:")
+            print(temp)"""
+            """set the number at that position to zero"""
+            tempMatrix.tile_seq[zeroLocationXCord][zeroLocationYCord + 1] = 0
+            """replace the 0 in the old position with the temp number, completing the swap"""
+            tempMatrix.tile_seq[zeroLocationXCord][zeroLocationYCord] = temp
+            """print(tempMatrix.tile_seq)"""
+            """self.openlist.append(tempMatrix)"""
+            self.check_inclusive(tempMatrix)
+
+            """if Y cord is greater than 0, you can perform a right swap"""
+            if zeroLocationYCord > 0:
+                print("I am doing a left swap")
+                print(zeroLocationXCord)
+                print(zeroLocationYCord)
+                """print(self.current.tile_seq)"""
+                """create a temp matrix to overwrite when performing the swap"""
+                tempMatrix = copy.deepcopy(self.current)
+                """Create a variable to hold the number of the position where zero is moving to"""
+                temp = tempMatrix.tile_seq[zeroLocationXCord][zeroLocationYCord - 1]
+                """print("Current temp is:")
+                print(temp)"""
+                """set the number at that position to zero"""
+                tempMatrix.tile_seq[zeroLocationXCord][zeroLocationYCord - 1] = 0
+                """replace the 0 in the old position with the temp number, completing the swap"""
+                tempMatrix.tile_seq[zeroLocationXCord][zeroLocationYCord] = temp
+                """print(tempMatrix.tile_seq)"""
+                """self.openlist.append(tempMatrix)"""
+                self.check_inclusive(tempMatrix)
+
+        """Pop the front of the list to continue"""
+        self.current = self.openlist.pop()
     #TODO your code end here
 
 
@@ -156,6 +204,8 @@ class UninformedSearchSolver:
             self.state_walk()
             print(self.current.tile_seq)
             path += 1
+
+
 
         print("It took ", path, " iterations")
         print("The length of the path is: ", self.current.depth)
